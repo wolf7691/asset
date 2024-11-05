f37d89b6af8751bdc61a4f799de35f7a9b76ff6e
wolf7691 <wolfgang.wakolbinger@outlook.com>
2024-11-05 16:34:37 +0100

    First adaptions

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Image from 'next/image';
 
 export default function Home() {
   noStore();
-  const timeOnServer = new Date().toLocaleTimeString('en-US');
+  const timeOnServer = new Date().toUTCString();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        This is a Next.js application hosted on Azure Static Web Apps with
-        hybrid rendering. The time on the server is{' '}
-        <strong>{timeOnServer}</strong>.
+        <center>
+          This is a Next.js application hosted on Azure Static Web Apps with
+          hybrid rendering.
+        </center>
+        <center>
+          The date and time on the server is <strong>{timeOnServer}</strong>.
+        </center>
       </div>
     </main>
   );
